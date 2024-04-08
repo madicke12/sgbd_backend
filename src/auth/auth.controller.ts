bcb4b6f4707a3c891c865f946b048e3c4a6e5705
madicke12 <cissemadicke8@gmail.com>
2024-04-08 15:44:44 +0000

    completed the signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthSignUpDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,9 @@ export class AuthController {
   @Post('logout')
   logout() {
     return this.authservice.logout();
+  }
+  @Post('signup')
+  signup(@Body() dto: AuthSignUpDto) {
+    return this.authservice.signup(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,39 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable({})
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthSignUpDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   login() {
     return { message: 'Login' };
   }
   logout() {
     return { message: 'Logout' };
+  }
+  async signup(dto: AuthSignUpDto) {
+    const hash = await argon.hash(dto.mot_de_passe);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          mot_de_passe: hash,
+          Prenom: dto.prenom,
+          nom: dto.nom,
+          role: dto.role,
+        },
+      });
+      delete user.mot_de_passe;
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Email deja prise');
+        }
+      }
+      throw e;
+    }
   }
 }
