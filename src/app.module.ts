0f3cd826a616cc695ef42a759820c7c8d35d5066
madicke12 <cissemadicke8@gmail.com>
2024-04-09 18:41:04 +0000

    w

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { RapportController } from './rapport/rapport.controller';
+import { AvisController } from './avis/avis.controller';
+import { RapportService } from './rapport/rapport.service';
+import { AvisService } from './avis/avis.service';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, RapportController, AvisController],
+  providers: [AppService, RapportService, AvisService],
 })
 export class AppModule {}
---
+++ src/rapport/rapport.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('rapport')
+export class RapportController {
+    @Get('all')
+    getAllRapport(){
+        return this.s
+    }
+}
